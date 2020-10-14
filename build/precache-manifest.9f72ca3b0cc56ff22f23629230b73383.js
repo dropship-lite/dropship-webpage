2c191389459d6cd07a7634946ff01dc6ed2dbe40
dropship.dev <>
2020-10-15 04:59:08 +0800

    git domain

---
+++ build/precache-manifest.9f72ca3b0cc56ff22f23629230b73383.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5312d7721c97839a389c3404012972",
+    "url": "/dropship-webpage/index.html"
+  },
+  {
+    "revision": "c43dc373f252616fec42",
+    "url": "/dropship-webpage/static/css/main.33ee9e55.chunk.css"
+  },
+  {
+    "revision": "0bd6dd2c1d146e8e6146",
+    "url": "/dropship-webpage/static/js/2.96bf28be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dropship-webpage/static/js/2.96bf28be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43dc373f252616fec42",
+    "url": "/dropship-webpage/static/js/main.47f1a50e.chunk.js"
+  },
+  {
+    "revision": "b5b6f74d8156f4d4d21d",
+    "url": "/dropship-webpage/static/js/runtime-main.2dd08d16.js"
+  },
+  {
+    "revision": "b331f0e7e48dec471716e3f857a576a3",
+    "url": "/dropship-webpage/static/media/bg2.b331f0e7.webp"
+  },
+  {
+    "revision": "9711bd42a27a014053e6c74d23b1ef48",
+    "url": "/dropship-webpage/static/media/dropship-logo.9711bd42.png"
+  },
+  {
+    "revision": "cc77168da9485f186100245b7afe3403",
+    "url": "/dropship-webpage/static/media/logo.cc77168d.gif"
+  },
+  {
+    "revision": "a92c53beba82cd247020318867b8792f",
+    "url": "/dropship-webpage/static/media/paypal.a92c53be.png"
+  }
+]);
