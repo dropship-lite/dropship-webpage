b02bb3df858ff5046844f6f2c0105884e38e0b31
Dropship Lite <70704616+dropship-lite@users.noreply.github.com>
2020-11-18 07:37:12 +0800

    Updates

---
+++ precache-manifest.66c5e89411594e7184862071c9104f77.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451b12065117229d933cdf7f3f8d5f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252338a75a9214bafbd3",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ccf6263da8594d03e975",
+    "url": "/static/css/main.40f0695f.chunk.css"
+  },
+  {
+    "revision": "252338a75a9214bafbd3",
+    "url": "/static/js/2.97482a6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97482a6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccf6263da8594d03e975",
+    "url": "/static/js/main.2a8d2835.chunk.js"
+  },
+  {
+    "revision": "e83a818b61b9e3c986db",
+    "url": "/static/js/runtime-main.2f15d624.js"
+  },
+  {
+    "revision": "fd36c99f51c12904b6a65df6b908195f",
+    "url": "/static/media/agent.fd36c99f.png"
+  },
+  {
+    "revision": "7f20f2f747522f282988af7a2590a275",
+    "url": "/static/media/android.7f20f2f7.png"
+  },
+  {
+    "revision": "c6455320fb49600de7653747a4f79eb4",
+    "url": "/static/media/bg.c6455320.jpg"
+  },
+  {
+    "revision": "ad3b946b0e29684141397471d4ed85df",
+    "url": "/static/media/chat.ad3b946b.png"
+  },
+  {
+    "revision": "9711bd42a27a014053e6c74d23b1ef48",
+    "url": "/static/media/dropship-logo.9711bd42.png"
+  },
+  {
+    "revision": "b3d620693943afb7b5a9dd878adc92d5",
+    "url": "/static/media/home-daily.b3d62069.png"
+  },
+  {
+    "revision": "f2df97d79e4cee6b0768798e0a2c4ef4",
+    "url": "/static/media/home-month.f2df97d7.png"
+  },
+  {
+    "revision": "50adb32612a850ca70c5fe249190d3ae",
+    "url": "/static/media/home-new-dark.50adb326.png"
+  },
+  {
+    "revision": "41710fdc9b5d1e363afc9748d26cc372",
+    "url": "/static/media/login.41710fdc.png"
+  },
+  {
+    "revision": "cc77168da9485f186100245b7afe3403",
+    "url": "/static/media/logo.cc77168d.gif"
+  },
+  {
+    "revision": "582fa5b2fc76c9e16c1a115cdd7e3494",
+    "url": "/static/media/more-arrow.582fa5b2.png"
+  },
+  {
+    "revision": "a92c53beba82cd247020318867b8792f",
+    "url": "/static/media/paypal.a92c53be.png"
+  },
+  {
+    "revision": "8da0f21836a4c887f2731c74c6956d71",
+    "url": "/static/media/profile.8da0f218.png"
+  },
+  {
+    "revision": "bbf1c69251ddbe457d7d29f2f85c638c",
+    "url": "/static/media/reg-stockist.bbf1c692.png"
+  },
+  {
+    "revision": "05ea6c168d56cbbe37c1d1299e658d5c",
+    "url": "/static/media/rq-item.05ea6c16.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dropship-webpage/precache-manifest.e136316ffa06ab53c8fdf7591492eeb6.js"
+  "/precache-manifest.66c5e89411594e7184862071c9104f77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dropship-webpage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
